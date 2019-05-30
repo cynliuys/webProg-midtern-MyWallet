fe93031b3498deb99d29f2fd3cb443e56870cea6
Cynthia <b04901152@ntu.edu.tw>
2019-05-30 13:01:19 +0800

    1

--- client/src/component/AddWindow.js
+++ client/src/component/AddWindow.js
@@ -212,8 +212,8 @@ class AddWindow extends React.Component{
                         index={this.state.value}
                         onChangeIndex={this.handleChangeIndex}
                     >
-                        <TabContainer dir={theme.direction}></TabContainer>
-                        <TabContainer dir={theme.direction}></TabContainer>
+                        <TabContainer dir={theme.direction}>Item One</TabContainer>
+                        <TabContainer dir={theme.direction}>Item Two</TabContainer>
                     </SwipeableViews> 
                     {/*
                     <Tabs
